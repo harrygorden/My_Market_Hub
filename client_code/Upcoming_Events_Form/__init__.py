471de8bd3e8a48e696961b346056af50320a6d89
harrygorden@gmail.com <harrygorden@gmail.com>
2025-03-15 17:07:34 +0000

    Created Upcoming_Events_Form; Edited Key_Lines_Form and Navigation; Deleted Form1

--- client_code/Upcoming_Events_Form/__init__.py
+++ client_code/Upcoming_Events_Form/__init__.py
@@ -1,14 +1,14 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import Upcoming_Events_FormTemplate
 from anvil import *
-import anvil.server
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.server
 
 
-class Form1(Form1Template):
+class Upcoming_Events_Form(Upcoming_Events_FormTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
